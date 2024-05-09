5748c691867212546e037faf94854ba162b5a1d3
vivek sutar <157276925+VivekSutar21@users.noreply.github.com>
2024-05-09 04:31:21 +0000

    syntax check

---
+++ 01basics/js1.js
@@ -0,0 +1,8 @@
+let username = "vivek"
+let password = "vk"
+
+if(username == 'vivek' && password == 'vk' ){
+    console.log("Welcome buddy.....")
+}else{
+    console.log("Chal hattt yaha se..")
+}
