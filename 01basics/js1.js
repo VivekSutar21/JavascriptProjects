4a5bcbf75a7c37b1c464152df56cca8eed2825b0
vivek sutar <157276925+VivekSutar21@users.noreply.github.com>
2024-05-09 11:10:57 +0000

    variable types declaration

--- 01basics/js1.js
+++ 01basics/js1.js
@@ -1,3 +1,11 @@
+const accountId = 123
+//this variable is declared using const (const variable are unchangable)
+let accountName ="vivek sutar"
+//this variable is declared using let (let variable are normal variable which is changable)
+var accountType = "saving"
+//this variable is declared using var (var variable are normal variable but we do not use var to declare because of scope problem)
+
+
 let username = "vivek"
 let password = "vk"
 
